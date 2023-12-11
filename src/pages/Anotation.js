5a7ac80040680709abc46c1ee0c47fc27222ee24
fernandopdeandrade <pupygreen@gmail.com>
2023-12-10 21:58:51 -0300

    feat: Criação de lógica para somar todas as contas e mostrar para o usuário!

--- src/pages/Anotation.js
+++ src/pages/Anotation.js
@@ -1,23 +1,31 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useEffect, useState } from 'react';
-import { Alert, Button, Pressable, ScrollView, Text, TextInput, View } from 'react-native';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import styles from '../styles/AnotacaoCss';
-import FormatData from '../utils/FormatData';
-import getAsyncStorage from '../utils/GetAsyncStorage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Button,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import styles from "../styles/AnotacaoCss";
+import FormatData from "../utils/FormatData";
+import getAsyncStorage from "../utils/GetAsyncStorage";
 
-const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
+const Anotation = ({ navigation, setLoading, setEdit, edit, indexData }) => {
   const [anotation, setAnotation] = useState([]);
-  const [body, setBody] = useState('');
-  const [title, setTitle] = useState('');
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
   const [date, setDate] = useState(new Date());
-  const [time, setTime] = useState('');
+  const [time, setTime] = useState("");
   const [showPicker, setShowPicker] = useState(false);
   const [messageHourCorrect, setMessageHourCorrect] = useState(false);
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
 
   useEffect(() => {
     handleChangeText();
@@ -28,12 +36,12 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
 
     setTimeout(() => {
       setLoading(false);
-    }, 1000)
+    }, 1000);
   }, []);
 
   const togglePicker = () => {
     setShowPicker(!showPicker);
-  }
+  };
 
   const onChange = (_event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -52,7 +60,7 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
   const getStorageSave = async () => {
     try {
       const value = await getAsyncStorage("anotation");
-      console.log('Sou o value do getStorageSave do Anotation', value);
+      console.log("Sou o value do getStorageSave do Anotation", value);
       const valueName = await getAsyncStorage("userName");
       setUserName(valueName);
       if (value !== null) {
@@ -75,7 +83,7 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
 
   const handleChangeText = () => {
     const regexHourMinutes = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
-    
+
     if (regexHourMinutes.test(time)) {
       setTime(time);
       setMessageHourCorrect(true);
@@ -85,22 +93,22 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
   };
 
   const verifyAnotation = () => {
-    if (title === '' || body === '' || date === '' || time === '') {
+    if (title === "" || body === "" || date === "" || time === "") {
       Alert.alert(
-        'Atenção!',
-        'Você precisa preencher todos os campos para salvar sua anotação!',
+        "Atenção!",
+        "Você precisa preencher todos os campos para salvar sua anotação!",
         [
           {
-            text: 'Ok',
-            onPress: () => console.log('Ok Pressed'),
-            style: 'cancel'
-          }
+            text: "Ok",
+            onPress: () => console.log("Ok Pressed"),
+            style: "cancel",
+          },
         ]
-      )
+      );
     } else {
       saveAnotation();
     }
-  }
+  };
 
   const updateAnotation = async () => {
     if (edit) {
@@ -109,32 +117,32 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
         body: body,
         date: FormatData(date),
         hour: time,
-      }
+      };
       const anotationUpdate = anotation;
       anotationUpdate.splice(indexData, 1, anotationEdit);
       setAnotation(anotationUpdate);
       setItem("anotation", anotationUpdate);
       setEdit(false);
-      setBody('');
-      setTitle('');
+      setBody("");
+      setTitle("");
       setDate(new Date());
-      setTime('');
+      setTime("");
     }
   };
 
   const saveAnotation = async () => {
     if (!messageHourCorrect) {
       Alert.alert(
-        'Atenção!',
-        'Você precisa preencher o horário corretamente!',
+        "Atenção!",
+        "Você precisa preencher o horário corretamente!",
         [
           {
-            text: 'Ok',
-            onPress: () => console.log('Ok Pressed'),
-            style: 'cancel'
-          }
+            text: "Ok",
+            onPress: () => console.log("Ok Pressed"),
+            style: "cancel",
+          },
         ]
-      )
+      );
       return;
     }
 
@@ -151,7 +159,7 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
         date: FormatData(date),
         hour: time,
         check: false,
-      }
+      },
     ]);
 
     setItem("anotation", [
@@ -162,105 +170,115 @@ const Anotation = ({navigation, setLoading, setEdit, edit, indexData}) => {
         date: FormatData(date),
         hour: time,
         check: false,
-      }
+      },
     ]);
 
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-      setBody('');
-      setTitle('');
+      setBody("");
+      setTitle("");
       setDate(new Date());
-      setTime('');
-    }, 1000)
-  }
+      setTime("");
+    }, 1000);
+  };
 
   const toGoBack = () => {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
     }, 1000);
-    navigation.navigate('Visualizar');
-  }
+    navigation.navigate("Visualizar");
+  };
 
   return (
     <ScrollView>
       <Header userName={userName} />
       <View style={styles.container}>
-        <Text style={{color: '#069', fontSize: 15, marginTop: 20}}>Insira suas anotações!</Text>
+        <Text style={{ color: "#069", fontSize: 15, marginTop: 20 }}>
+          Insira suas anotações!
+        </Text>
         <TextInput
           style={styles.inputTitle}
           value={title}
           onChangeText={(title) => setTitle(title)}
-          placeholder='Digite o título da sua anotação aqui...'
+          placeholder="Digite o título da sua anotação aqui..."
         />
         <TextInput
           style={styles.input}
           value={body}
           onChangeText={(body) => setBody(body)}
-          placeholder='Digite sua anotação aqui...'
+          placeholder="Digite a descrição aqui..."
           multiline={true}
         />
         <View style={styles.containerData}>
           <LinearGradient
-              colors={['#4c669f', '#3b5998', '#192f6a']}
-              style={{top: 0, left: 0, right: 0, position: 'absolute', height: '100%'}}/> 
+            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            style={{
+              top: 0,
+              left: 0,
+              right: 0,
+              position: "absolute",
+              height: "100%",
+            }}
+          />
           <Pressable onPress={togglePicker}>
             <View style={styles.viewDataInputText}>
-            <Text style={{color: 'white'}}>Clique e selecione uma data: </Text>
-              <TextInput 
-              style={{color: 'white', fontWeight: 'bold', marginLeft: 10}}
-              value={date.toLocaleDateString()}
-              placeholder='Selecione a data'
-              placeholderTextColor='red'
-              editable={false}
-              onChangeText={(date) => setDate(date)}
-            />            
+              <Text style={{ color: "white" }}>
+                Clique e selecione uma data:{" "}
+              </Text>
+              <TextInput
+                style={{ color: "white", fontWeight: "bold", marginLeft: 10 }}
+                value={date.toLocaleDateString()}
+                placeholder="Selecione a data"
+                placeholderTextColor="red"
+                editable={false}
+                onChangeText={(date) => setDate(date)}
+              />
             </View>
           </Pressable>
-          {showPicker && (<DateTimePicker
-            value={date}
-            display='spinner'
-            mode="date"
-            placeholder="Selecione a data"
-            format="DD-MM-YYYY"
-            minimunDate={new Date(2020, 1, 1)}
-            maximumDate={new Date(2025, 12, 31)}
-            confirmBtnText="Confirmar"
-            cancelBtnText="Cancelar"
-            is24Hour={true}
-            onPress={togglePicker}
-            onChange={onChange}
-          />)}
+          {showPicker && (
+            <DateTimePicker
+              value={date}
+              display="spinner"
+              mode="date"
+              placeholder="Selecione a data"
+              format="DD-MM-YYYY"
+              minimunDate={new Date(2020, 1, 1)}
+              maximumDate={new Date(2025, 12, 31)}
+              confirmBtnText="Confirmar"
+              cancelBtnText="Cancelar"
+              is24Hour={true}
+              onPress={togglePicker}
+              onChange={onChange}
+            />
+          )}
         </View>
         <View style={styles.viewHours}>
+          <Text>Formato das horas: 00:00</Text>
           <TextInput
-          style={styles.inputHours}
-          placeholder="Horário"
-          value={time}
-          onChangeText={(time) => setTime(time)}
+            style={styles.inputHours}
+            placeholder="Horário"
+            value={time}
+            onChangeText={(time) => setTime(time)}
           />
           <View>
-            {messageHourCorrect
-              ? (<Text style={styles.correctHour}>Horário correto</Text>)
-              : (<Text style={styles.incorrectHour}>Horário incorreto</Text>)}
+            {messageHourCorrect ? (
+              <Text style={styles.correctHour}>Horário correto</Text>
+            ) : (
+              <Text style={styles.incorrectHour}>Horário incorreto</Text>
+            )}
           </View>
-        </View>      
-        <View style={styles.viewButtons}>
-          <Button
-            title='Ver anotações'
-            onPress={() => toGoBack()} />
-
-          <Button
-            color= 'green'
-            title='Salvar'
-            onPress={verifyAnotation} />      
         </View>
-      </View>      
+        <View style={styles.viewButtons}>
+          <Button title="Ver anotações" onPress={() => toGoBack()} />
+
+          <Button color="green" title="Salvar" onPress={verifyAnotation} />
+        </View>
+      </View>
       <Footer />
     </ScrollView>
-      
-  )
-}
+  );
+};
 
 export default Anotation;
--- src/pages/Read.js
+++ src/pages/Read.js
@@ -1,18 +1,28 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useEffect, useState } from 'react';
-import { Alert, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import styles, { ids } from '../styles/LeituraCss';
-import getAsyncStorage from '../utils/GetAsyncStorage';
-import SortTarefas from '../utils/SortTarefas';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import styles, { ids } from "../styles/LeituraCss";
+import getAsyncStorage from "../utils/GetAsyncStorage";
+import SortTarefas from "../utils/SortTarefas";
 
 const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
   const [anotation, setAnotation] = useState([]);
   const [orderStorage, setOrderStorage] = useState(false);
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
+  const [numbersSum, setNumbersSum] = useState([]);
+  console.log("numbersSum: ", numbersSum);
+  console.log("anotation: ", anotation.length);
 
   useEffect(() => {
     (async () => {
@@ -22,11 +32,15 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
       if (value !== null) {
         setAnotation(value);
       }
+
+      if (anotation.length > 0) {
+        sumNumbers(anotation);
+      }
     })();
 
     setTimeout(() => {
       setLoading(false);
-    }, 1000)
+    }, 1000);
   }, [loading]);
 
   const order = async () => {
@@ -34,7 +48,7 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
       const orderData = SortTarefas(anotation);
       if (orderData === null) {
         return;
-      } 
+      }
       setOrderStorage(!orderStorage);
       setAnotation(orderData);
       await AsyncStorage.setItem("anotation", JSON.stringify(orderData));
@@ -42,50 +56,46 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
       console.log(error);
     }
   };
-    
+
   useEffect(() => {
     if (orderStorage) {
-      Alert.alert(
-        'Atenção!',
-        'Você deseja ordenar as tarefas?',
-        [
-          {
-            text: 'Cancelar',
-            onPress: () => console.log('Cancel Pressed'),
-            style: 'cancel'
+      Alert.alert("Atenção!", "Você deseja ordenar as tarefas?", [
+        {
+          text: "Cancelar",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "Ordenar",
+          onPress: () => {
+            order();
           },
-          {
-            text: 'Ordenar',
-            onPress: () => {
-              order();
-            }
-          }
-        ]
-      )
+        },
+      ]);
     }
   }, [orderStorage]);
 
   const goAnotation = () => {
     setLoading(true);
-    navigation.navigate('Criar');
+    navigation.navigate("Criar");
   };
 
   const editIndex = (index) => {
-    console.log('Sou o index do editIndex', index);
+    console.log("Sou o index do editIndex", index);
     setEdit(true);
     setIndexData(index);
-    navigation.navigate('Criar');
+    navigation.navigate("Criar");
   };
 
   const clearAsyncStorageIndex = (index) => {
     anotation.splice(index, 1);
     setAnotation(anotation);
     AsyncStorage.setItem("anotation", JSON.stringify(anotation));
-    
+
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 1000)
+    }, 1000);
   };
 
   const chekOrNotCheck = (ele, index) => {
@@ -97,7 +107,7 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
       setLoading(true);
       setTimeout(() => {
         setLoading(false);
-      }, 1000)
+      }, 1000);
     } else {
       ele.check = false;
       anotation.splice(index, 1, ele);
@@ -106,22 +116,22 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
       setLoading(true);
       setTimeout(() => {
         setLoading(false);
-      }, 1000)
+      }, 1000);
     }
   };
 
   const clearAsyncStorage = async () => {
     Alert.alert(
-      'Atenção!',
-      'Você tem certeza que deseja limpar todas as anotações?',
+      "Atenção!",
+      "Você tem certeza que deseja limpar todas as anotações?",
       [
         {
-          text: 'Cancelar',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel'
+          text: "Cancelar",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
         },
         {
-          text: 'Limpar',
+          text: "Limpar",
           onPress: () => {
             AsyncStorage.clear();
             setAnotation([]);
@@ -129,86 +139,153 @@ const Read = ({ navigation, setLoading, loading, setIndexData, setEdit }) => {
             setLoading(true);
             setTimeout(() => {
               setLoading(false);
-            }, 1000)
-          }
-        }
+            }, 1000);
+          },
+        },
       ]
-    )
+    );
+  };
+
+  function sumNumbers(anotation) {
+    // Filtra o array para obter apenas os objetos com números no campo body
+    const filteredAnotation = anotation.filter((item) => {
+      const body = item.body;
+      return typeof body === "string" && body.match(/[0-9]+.?[0-9]*/g);
+    });
+
+    // Extrai os números de cada objeto e some-os
+    const sum = filteredAnotation.reduce((acc, item) => {
+      const numbers = item.body
+        .match(/[0-9]+.?[0-9]*/g)
+        .map((number) => Number(number));
+      return acc + numbers.reduce((a, b) => a + b, 0);
+    }, 0);
+
+    // Formata o resultado
+    setNumbersSum(sum.toFixed(2));
   }
 
   return (
     <ScrollView>
       <Header userName={userName} />
-     <View style={styles.containerLeitura}>
-      <Text style={styles.textH2}>Minhas tarefas</Text>
-      {
-        anotation !== null && anotation.length > 0
-          ?
-          (<View style={styles.infoAnotation}>{anotation.map((ele, index) => {
-            return (
-              <View key={index} style={styles.titleDescription}>
-                <Image style={styles.imagePapel} source={require('../images/fundo-papel.jpg')} dataSet={{ media: ids.imagePapel }}/>
-                <View style={styles.viewCheched}>
-                  {
-                    !ele.check
-                      ? <Text style={{ color: 'red', marginRight: 10, fontWeight: 900, fontSize: 16, marginTop: 5 }}>Concluír</Text>
-                      : <Text style={{ color: 'green', marginRight: 10, fontWeight: 900, fontSize: 16, marginTop: 5 }}>Concluída</Text>
-                  }                    
-                  <Icon
-                    name={ele.check ? 'check' : 'circle-thin'}
-                    size={30}
-                    color={ele.check ? 'green' : 'red'}
-                    onPress={() => chekOrNotCheck(ele, index)} />             
+      <View style={styles.containerLeitura}>
+        <Text style={styles.textH2}>Minhas tarefas</Text>
+        <Text style={styles.textH3}>
+          Se houver contas a pagar, insira na descrição!
+        </Text>
+        <Text style={styles.textH3}>
+          Total das contas somadas: {anotation.length > 0 ? numbersSum : "0"} R$
+        </Text>
+        {anotation !== null && anotation.length > 0 ? (
+          <View style={styles.infoAnotation}>
+            {anotation.map((ele, index) => {
+              return (
+                <View key={index} style={styles.titleDescription}>
+                  <Image
+                    style={styles.imagePapel}
+                    source={require("../images/fundo-papel.jpg")}
+                    dataSet={{ media: ids.imagePapel }}
+                  />
+                  <View style={styles.viewCheched}>
+                    {!ele.check ? (
+                      <Text
+                        style={{
+                          color: "red",
+                          marginRight: 10,
+                          fontWeight: 900,
+                          fontSize: 16,
+                          marginTop: 5,
+                        }}
+                      >
+                        Concluír
+                      </Text>
+                    ) : (
+                      <Text
+                        style={{
+                          color: "green",
+                          marginRight: 10,
+                          fontWeight: 900,
+                          fontSize: 16,
+                          marginTop: 5,
+                        }}
+                      >
+                        Concluída
+                      </Text>
+                    )}
+                    <Icon
+                      name={ele.check ? "check" : "circle-thin"}
+                      size={30}
+                      color={ele.check ? "green" : "red"}
+                      onPress={() => chekOrNotCheck(ele, index)}
+                    />
+                  </View>
+                  <View style={styles.viewTitle}>
+                    <Text style={styles.titleP}>Título: {ele.title}</Text>
+                  </View>
+                  <View style={styles.viewDescription}>
+                    <Text style={styles.descDescription}>Descrição</Text>
+                    <Text style={styles.eleDescription}>
+                      <Icon name="pencil" size={18} color="#900" /> {ele.body}
+                    </Text>
+                  </View>
+                  <View style={styles.viewDate}>
+                    <Text style={styles.dateDescription}>Hora: {ele.hour}</Text>
+                  </View>
+                  <View style={styles.viewDate}>
+                    <Text style={styles.dateDescription}>Data: {ele.date}</Text>
+                  </View>
+                  <View style={styles.viewButton}>
+                    <TouchableOpacity
+                      style={styles.buttonEditar}
+                      onPress={() => editIndex(index)}
+                    >
+                      <Text style={styles.editar}>Editar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.buttonExcluir}
+                      onPress={() => clearAsyncStorageIndex(index)}
+                    >
+                      <Text style={styles.excluir}>Excluir</Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
-                <View style={styles.viewTitle}><Text style={styles.titleP}>Título: {ele.title}</Text>
-                </View>
-                <View style={styles.viewDescription}>
-                  <Text style={styles.descDescription}>Descrição</Text>
-                  <Text style={styles.eleDescription}>
-                    <Icon name="pencil" size={18} color="#900" /> {ele.body}
-                  </Text>
-                </View>
-                <View style={styles.viewDate}>
-                  <Text style={styles.dateDescription}>Hora: {ele.hour}</Text>
-                </View>
-                <View style={styles.viewDate}>
-                  <Text style={styles.dateDescription}>Data: {ele.date}</Text>
-                </View>
-                <View style={styles.viewButton}>
-                <TouchableOpacity style={styles.buttonEditar} onPress={() => editIndex(index)}>
-                  <Text style={styles.editar}>Editar</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonExcluir} onPress={() => clearAsyncStorageIndex(index)}>
-                  <Text style={styles.excluir}>Excluir</Text>
-                </TouchableOpacity>
-                </View>
-              </View>
-            )
-          })}
-        </View>)
-        : (<Text style={styles.textMessageWarning}>Nenhuma tarefa encontrada!</Text>)
-      }
-      <TouchableOpacity style={styles.btnLeitura} onPress={goAnotation}>
-        <LinearGradient
-          colors={['#4c669f', '#3b5998', '#192f6a']}
-          style={{ top: 0, left: 0, right: 0, position: 'absolute', height: '100%', borderRadius: 35}} />
-       <Text style={styles.mais}>+</Text>
-      </TouchableOpacity>
+              );
+            })}
+          </View>
+        ) : (
+          <Text style={styles.textMessageWarning}>
+            Nenhuma tarefa encontrada!
+          </Text>
+        )}
+        <TouchableOpacity style={styles.btnLeitura} onPress={goAnotation}>
+          <LinearGradient
+            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            style={{
+              top: 0,
+              left: 0,
+              right: 0,
+              position: "absolute",
+              height: "100%",
+              borderRadius: 35,
+            }}
+          />
+          <Text style={styles.mais}>+</Text>
+        </TouchableOpacity>
         <View style={styles.viewOrder}>
-        <TouchableOpacity style={styles.btnOrder} onPress={() => setOrderStorage(true)}>
-        <Text style={styles.orderText}>Ordenar lista</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnClear} onPress={clearAsyncStorage}>
-        <Text style={styles.orderText}>Excluir tudo</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.btnOrder}
+            onPress={() => setOrderStorage(true)}
+          >
+            <Text style={styles.orderText}>Ordenar lista</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnClear} onPress={clearAsyncStorage}>
+            <Text style={styles.orderText}>Excluir tudo</Text>
+          </TouchableOpacity>
         </View>
-      </View>     
-    <Footer />
+      </View>
+      <Footer />
     </ScrollView>
-
-  )
-}
+  );
+};
 
 export default Read;
-
-
